089b4f61a0779e099bffc1f92ee7ffee8534caf4
mooky <mooky1007@naver.com>
2024-04-25 17:16:54 +0900

    시스템 테스트 로직 추가

--- casino.js
+++ casino.js
@@ -7,7 +7,13 @@ class Casino {
         this.record = [];
         this.record2 = [];
         this.maxium = 0;
-        this.firstCoin = 100000;
+        this.unit = 100;
+        this.firstCoin = 1000000;
+
+        this.gameTime = 1500;
+        this.resultTime = 1000;
+
+        this.gameCount = 0;
 
         this.init();
         this.render();
@@ -58,103 +64,117 @@ class Casino {
     }
 
     play(value) {
-        if (this.bet === 0) {
-            console.log('배팅 금액을 선택하세요.');
-            return;
-        }
-
-        if (this.isPlay) {
-            console.log('게임 중입니다.');
-            return;
-        }
-        if (this.coin < this.bet) {
-            console.log('코인이 부족합니다.');
-            return;
-        }
-
-        this.coin -= this.bet;
-        this.isPlay = true;
-        this.lastBet = this.bet;
-        this.render();
-
-        // betMsg.innerHTML = '게임 중...';
-
-        for (let i = 0; i < 150; i++) {
-            setTimeout(() => {
-                let ranNum = Math.floor(Math.random() * 100) + 1;
-                betMsg.innerHTML = `<span style="font-size: 36px; color: ${ranNum % 2 == 0 ? '#4e81b4' : '#fc8242'};">${ranNum}</span>`;
-            }, i * 10);
-        }
-
-        setTimeout(() => {
-            let number = Math.floor(Math.random() * 100) + 1;
-            let result = number % 2 == 0 ? 2 : 1;
-
-            this.record.push(result === 1 ? '홀' : '짝');
-            if (this.record.length > 9) {
-                this.record.shift();
+        return new Promise((resolve, reject) => {
+            if (this.bet === 0) {
+                console.log('배팅 금액을 선택하세요.');
+                return;
             }
 
-            const date = new Date();
-
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
-            const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
-            const hours = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
-            const minutes = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
-            const seconds = date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds();
-
-            this.record2.push({
-                date: `${year}. ${month}. ${day} ${hours}:${minutes}:${seconds}`,
-                price: value === result ? this.bet : this.bet * -1,
-                result: value === result ? '승' : '패',
-            });
-            if (this.record2.length > 100) {
-                this.record2.shift();
+            if (this.isPlay) {
+                console.log('게임 중입니다.');
+                return;
+            }
+            if (this.coin < this.bet) {
+                console.log('코인이 부족합니다.');
+                return;
             }
 
-            if (value == result) {
-                this.coin += this.bet * 2;
+            this.coin -= this.bet;
+            this.gameCount++;
+            this.isPlay = true;
+            this.lastBet = this.bet;
+            this.render();
+
+            // betMsg.innerHTML = '게임 중...';
+
+            for (let i = 0; i < this.gameTime/10; i++) {
+                setTimeout(() => {
+                    let ranNum = Math.floor(Math.random() * 37);
+                    betMsg.innerHTML = `<span style="font-size: 36px; color: ${ranNum % 2 == 0 ? '#4e81b4' : '#fc8242'};">${ranNum}</span>`;
+                }, i * 10);
             }
-
-            const resultMsg = `<span style="font-size: 36px; color: ${number % 2 == 0 ? '#4e81b4' : '#fc8242'};">${number}</span>`;
-            // this.modal(number, resultMsg, this.bet * 2);
-
-            betMsg.innerHTML = resultMsg;
-
-            const p = document.createElement('p');
-            p.style.cssText = `
-                display: flex;
-                flex-direction: column;
-                gap: 5px;
-                position: absolute;
-                top: 65%;
-            `;
-
-            const resultText = value === result ? '<span style="font-size:14px;">축하합니다.</span>' : '<span style="font-size:14px;">패배하셨습니다.</span>';
-            const resultPrice = value === result ? `+${this.bet.toLocaleString()}` : (this.bet * -1).toLocaleString();
-
-            p.innerHTML = resultText + `₩${resultPrice}`;
-
-            betMsg.appendChild(p);
-
-            this.bet = 0;
-
-            if (this.coin > this.maxium) {
-                this.maxium = this.coin;
-            }
-
-            this.isPlay = false;
 
             setTimeout(() => {
-                this.render();
-            }, 1000);
-        }, 1500);
+                let number = Math.floor(Math.random() * 37);
+                let result = number % 2 == 0 ? 2 : 1;
+
+                if (number !== 0) {
+                    this.record.push(number);
+                } else {
+                    this.record.push('0');
+                    result = 3;
+                }
+
+                if (this.record.length > 9) {
+                    this.record.shift();
+                }
+
+                const date = new Date();
+
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+                const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
+                const hours = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
+                const minutes = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
+                const seconds = date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds();
+
+                this.record2.push({
+                    date: `${year}. ${month}. ${day} ${hours}:${minutes}:${seconds}`,
+                    price: value === result ? this.bet : this.bet * -1,
+                    result: value === result ? '승' : '패',
+                });
+                if (this.record2.length > 200) {
+                    this.record2.shift();
+                }
+
+                if (value == result) {
+                    this.coin += this.bet * 2;
+                }
+
+                const resultMsg = `<span style="font-size: 36px; color: ${
+                    number == 0 ? '#000' : number % 2 == 0 ? '#4e81b4' : '#fc8242'
+                };">${number}</span>`;
+                // this.modal(number, resultMsg, this.bet * 2);
+
+                betMsg.innerHTML = resultMsg;
+
+                const p = document.createElement('p');
+                p.style.cssText = `
+                    display: flex;
+                    flex-direction: column;
+                    gap: 5px;
+                    position: absolute;
+                    top: 65%;
+                `;
+
+                const resultText =
+                    value === result ? '<span style="font-size:14px;">축하합니다.</span>' : '<span style="font-size:14px;">패배하셨습니다.</span>';
+                const resultPrice = value === result ? `+${this.bet.toLocaleString()}` : (this.bet * -1).toLocaleString();
+
+                p.innerHTML = resultText + `₩${resultPrice}`;
+
+                betMsg.appendChild(p);
+
+                this.bet = 0;
+
+                if (this.coin > this.maxium) {
+                    this.maxium = this.coin;
+                }
+
+                this.isPlay = false;
+                resolve(value === result ? true : false);
+                
+                setTimeout(() => {
+                    this.render();
+                }, this.resultTime);
+            }, this.gameTime);
+        });
     }
 
     render() {
         coin.innerHTML = `소지금: ₩${this.coin.toLocaleString()}`;
         // maxium.innerHTML = `최대 소지금: ₩${this.maxium.toLocaleString()}`;
+        // gameCount.innerHTML = `게임 횟수: ${this.gameCount}`;
 
         if (this.bet === 0) {
             if (this.coin === 0) {
@@ -173,10 +193,11 @@ class Casino {
                     this.coin = this.firstCoin;
                     this.maxium = this.coin;
                     this.lastBet = 0;
+                    this.gameCount = 0;
                     this.record = [];
                     this.record2 = [];
                     this.render();
-                    
+
                     window.open('https://link.coupang.com/a/bzdp8o', '_blank');
                 });
 
@@ -238,7 +259,17 @@ class Casino {
         record.innerHTML = [...this.record]
             .reverse()
             .map((item, index) => {
-                return `<span style="${item === '홀' ? 'color:#fc8242;' : 'color:#4e81b4;'}">${item}</span>`;
+                switch (item) {
+                    case '0':
+                        return `<span style="color:#000;">${item}</span>`;
+                        break;
+                    default:
+                        if (item % 2 == 0) {
+                            return `<span style="color:#4e81b4;">${item}</span>`;
+                        } else {
+                            return `<span style="color:#fc8242;">${item}</span>`;
+                        }
+                }
             })
             .join('');
 
@@ -261,28 +292,23 @@ class Casino {
         window.localStorage.setItem('record2', JSON.stringify(this.record2));
     }
 
-    modal(value, result) {
-        const div = document.createElement('div');
-        div.classList.add('modal');
+    async autoPlay(seed = this.unit) {
+        this.gameTime = 0;
+        this.resultTime = 0;
 
-        const h1 = document.createElement('h1');
-        h1.innerHTML = value;
-        h1.style.color = value % 2 == 0 ? '#4e81b4' : '#fc8242';
+        this.bet = seed;
+        const result = await this.play(1);
 
-        const p = document.createElement('p');
-        p.innerHTML = result;
-
-        div.append(h1, p);
-
-        // betMsg.appendChild(div);
-        document.body.appendChild(div);
-
-        setTimeout(() => {
-            div.style.opacity = 0;
-            div.style.top = '15.75%';
-            setTimeout(() => {
-                document.body.removeChild(div);
-            }, 500);
-        }, 1000);
+        if (result) {
+            this.autoPlay(this.unit);
+        } else {
+            if(this.autoWinCount === 0){
+                this.autoPlay(seed * 2 + this.unit);
+            }else{
+                this.autoWinCount = 0;
+                this.autoPlay(this.unit);
+            }
+            
+        }
     }
 }
